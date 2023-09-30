da8d924143d4d54e825d135866c87ede58358e6f
marissabrielle <marissabrielle@gmail.com>
2023-09-30 14:57:45 -0700

    coin flip game

--- js/03-coinflip.js
+++ js/03-coinflip.js
@@ -1,25 +1,30 @@
 // The “Coin Flip” Game
 
-// Usage: Use nested conditional statements
-
-// In this part of the assignment you will explore what you’ve learned so far about variables, prompts, conditionals, operators, math, and more to create a “coin flip game”. To complete the game follow the steps outlined below:
-
-// Begin your application by creating a variable called coinFlip and set it equal to a random number. You will have to use a Math method to get this number:
-
-// let randomNum = Math.round(Math.random())
+let coinFlip = Math.floor(Math.random() * 100) + 1
+// console.log(coinFlip)
 
 // Prompt the user to select “Heads or Tails” and set the result to a new variable called choice.
+let choice = prompt('Heads or Tails?')
 
 // Use a conditional to check the result of the coin flip. If it’s less than a certain number, it will be heads. If it’s greater than a certain number, it will be tails.
+if (choice == 'Heads' && coinFlip >= 50) {
+    document.write('The flip was heads and you chose heads...you win!')
+}
 
-// If the result is heads and the user selects heads, display the following message within an alert:
-// The flip was heads and you chose heads...you win!
+else if (choice == 'Tails' && coinFlip >= 50) {
+    document.write('The flip was heads but you chose tails...you lose!')
 
-// If the result is heads and the user selects tails, display the following message within an alert:
-// The flip was heads but you chose tails...you lose!
+}
+else if (choice == 'Heads' && coinFlip <=49) {
+    document.write('The flip was tails but you chose heads...you lose!')
+}
 
-// If the result is tails and the user selects heads, display the following message within an alert:
-// The flip was tails but you chose heads...you lose!
+else if (choice == 'Tails' && coinFlip <=49) {
+    document.write('The flip was tails and you chose tails...you win!')
+}
 
-// If the result is tails and the user selects tails, display the following message within an alert:
-// The flip was tails and you chose tails...you win!
+else {
+    alert('You did not select heads or tails')
+}
+
+// console.log(coinFlip)
