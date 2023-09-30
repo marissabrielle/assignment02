6ee23d7e8a2ed6c4f30e7d13faae60053dfdba3e
marissabrielle <marissabrielle@gmail.com>
2023-09-30 15:27:02 -0700

    grade assigner

--- js/04-grade-assigner.js
+++ js/04-grade-assigner.js
@@ -1,16 +1,37 @@
 // The “Grade Assigner” Application
 
-// Usage: Use else if or switch statements
-
 // Collect the a number between 1 and 100 from the user.
+let grade = prompt('Enter a number between 1 and 100')
+
 // Make sure that number is between 1 and 100, if it is not, let the user know that only numbers between 1 and 100 are accepted.
+if (grade < 1 || grade > 100) {
+    alert('Only numbers between 1 and 100 are accepted')
+}
 
-// If the score is between 60 and 69, write out “You received a D” into the console.
+if (isNaN(grade)) {
+    alert(grade + ' is not a number');
+}
 
-// If the score is between 70 and 79, write out “You received a C” into the console.
+switch (true) {
+	// If the score is between 90 and 100
+	case grade >= 90 && grade <= 100:
+		console.log('You received a A')
+		break;
+	// If the score is between 80 and 89
+	case grade >= 80 && grade <= 89:
+		console.log('You received a B')
+		break;
+	/// If the score is between 70 and 79
+	case grade >= 70 && grade <= 79:
+		console.log('You received a C')
+		break;
+	// If the score is between 60 and 69
+	case grade >= 60 && grade <= 69:
+		console.log('You received a D')
+		break;
 
-// If the score is between 80 and 89, write out “You received a B” into the console.
-
-// If the score is between 90 and 100, write out “You received an A” into the console.
-
-// If the score is less than 60, write out “You received an F” into the console.
+    // If the score is less than 60
+    case grade <= 59:
+        console.log('You received an F')
+        break;
+}
